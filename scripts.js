fb2a77205af96168d7ee0209f4aae475f944733f
unknown <nbatre1@yahoo.com>
2018-12-14 00:42:21 -0600

    keep moving forward

--- scripts.js
+++ scripts.js
@@ -1,8 +1,27 @@
 const cards = document.querySelectorAll('.memory-card');
 
+// storing cards
+
+let hasFlippedCard = false;
+let firstCard,secondCard;
+
 function flipcard (){
  this.classList.toggle('flip');
+
+ if(!hasFlippedCard){
+     // first click
+     hasFlippedCard =true;
+     firstCard = this;
+
+     console.log(hasFlippedCard , firstCard);
+ }
+  else{
+      //second card flip
+      hasFlippedCard =false;
+      secondCard = this
+  }
 }
+
 
 
 //loops through the card
